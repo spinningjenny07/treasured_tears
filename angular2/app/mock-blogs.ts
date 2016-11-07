5170dde54421132576dfb366f4ebeb249deb6394
Jenny Jones <spinningjenny07@gmail.com>
2016-11-07 10:44:33 -0700

    Add mock-blogs.ts

---
+++ angular2/app/mock-blogs.ts
@@ -0,0 +1,21 @@
+import { Blog } from './blog';
+
+export const BLOGS: Blog[] = [{
+		title: "Blog Post 1",
+		author: "Me",
+		date: new Date(),
+		summary: "#1",
+		content: "This is blog post #1!"
+	}, {
+		title: "Blog Post 2",
+		author: "Me",
+		date: new Date(),
+		summary: "#2",
+		content: "This is blog post #2!"
+	}, {
+		title: "Blog Post 3",
+		author: "Me",
+		date: new Date(),
+		summary: "#3",
+		content: "This is blog post #3!"
+	}];
